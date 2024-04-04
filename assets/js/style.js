71e974d94895802882c0f673108041108856c557
mdmechodal <152836120+mdmechodal@users.noreply.github.com>
2024-04-04 18:17:22 +0530

    first commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,3 +1,28 @@
+var owl = $(" .owl-carousell");
+owl.owlCarousel({
+  margin: 10,
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    475: {
+      items: 3,
+    },
+    600: {
+      items: 4,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+// class="owl-carousel owl-theme"
+// class="item"
+
+
 var owl = $(".owl-carousel");
 owl.owlCarousel({
   margin: 10,
@@ -15,7 +40,7 @@ owl.owlCarousel({
       items: 4,
     },
     1000: {
-      items: 7,
+      items: 1,
     },
   },
 });
