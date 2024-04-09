a7799ebd9d9a7cae5cec83c0bb3344af38937c2a
mdmechodal <152836120+mdmechodal@users.noreply.github.com>
2024-04-09 19:08:01 +0530

    first commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,4 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+   let obj = document.getElementById(id),
+    current = start,
+    range = end - start,
+    increment = end > start ? 1 : -1,
+    step = Math.abs(Math.floor(duration / range)),
+    timer = setInterval(() => {
+     current += increment;
+     obj.textContent = current;
+     if (current == end) {
+      clearInterval(timer);
+     }
+    }, step);
+  }
+  counter("count1", 0, 589, 1);
+  counter("count2", 100, 525, 2500);
+  counter("count3", 0, 274, 3000);
+  counter("count4", 0, 1248, 3000);
+  counter("count5", 0, 30, 3000);
+ });
+ 
 
+AOS.init();
 
 var owl = $(" .owl-carousell");
 owl.owlCarousel({
@@ -8,13 +31,13 @@ owl.owlCarousel({
   autoplayTimeout: 3000,
   responsive: {
     0: {
-      items: 2,
+      items: 1,
     },
     475: {
-      items: 3,
+      items: 2,
     },
     600: {
-      items: 4,
+      items: 3,
     },
     1000: {
       items: 3,
@@ -33,13 +56,13 @@ owl.owlCarousel({
   autoplayTimeout: 3000,
   responsive: {
     0: {
-      items: 2,
+      items: 1,
     },
     475: {
-      items: 3,
+      items: 1,
     },
     600: {
-      items: 4,
+      items: 1,
     },
     1000: {
       items: 1,
